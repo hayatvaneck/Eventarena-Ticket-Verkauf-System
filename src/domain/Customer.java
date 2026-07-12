93297405f6af06bdb0ada27fed952df2d27f8c58
Lukas <lukasbecktown@gmail.com>
2026-07-12 13:28:40 +0200

    Refactor Customer: Enum CustomerType und Validierung

--- src/domain/Customer.java
+++ src/domain/Customer.java
@@ -1,29 +1,43 @@
 package domain;
 
 public class Customer {
-    private final Long id;
+    private final long id;
     private final String firstName;
     private final String lastName;
-    private final String customerType; // Student, Rentern, VIP etc.
+    private final CustomerType customerType;
 
-    public Customer(Long id, String firstName, String lastName, String customerType){
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.customerType = customerType;
+    public Customer(long id, String firstName, String lastName, CustomerType customerType) {
+         if (id <= 0) {
+            throw new IllegalArgumentException("ID muss größer als 0 sein.");
+        }
+        if (firstName == null || firstName.isBlank()) {
+        throw new IllegalArgumentException("Vorname darf nicht leer sein.");
+    }
+        if (lastName == null || lastName.isBlank()) {
+        throw new IllegalArgumentException("Nachname darf nicht leer sein.");
+    }
+        if (customerType == null) {
+        throw new IllegalArgumentException("CustomerType darf nicht null sein.");
     }
 
-    // Getter
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.customerType = customerType;
+}
     public long getId() {
         return id;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
-    public String getCustomerType() {
+
+    public CustomerType getCustomerType() {
         return customerType;
     }
 
